cc3af75af896934e8a80e862aa7314519d44834e
yeah1832 <aer561@naver.com>
2025-04-10 17:20:48 +0900

    ncp 수정

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react()],
   build: {
     rollupOptions: {
